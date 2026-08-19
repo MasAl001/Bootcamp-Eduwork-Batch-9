12b9e0eb96e2fa62fd1f3aba883b0fdb36d5d7e9
MasAl001 <muhammadghozy15@gmail.com>
2026-08-19 14:04:26 +0700

    Add tugas sesi 15

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,15 @@ Route::get('/products/{slug}', [ProductController::class, 'show'])->name('view.p
 Route::get('/carts', function () {
     return view('carts', ['title' => 'Carts']);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
